152cba7bcbcac00299f8ac98107df1f859321679
JohnsonClayton <cpjohnson@mavs.coloradomesa.edu>
2020-02-16 18:49:32 -0700

    fixed dconf write bug and removed copy making

--- get_new_sun_image.sh
+++ get_new_sun_image.sh
@@ -5,15 +5,5 @@ if [[ -f latest_4096_0304.jpg ]] ; then
 fi
 
 wget https://sdo.gsfc.nasa.gov/assets/img/latest/latest_4096_0304.jpg
-cp latest_4096_0304.jpg /home/notclaytonjohnson/Pictures/Sun/0304/latest_4096_0304_$(date -u --iso-8601=minutes).jpg
-mv latest_4096_0304.jpg /home/notclaytonjohnson/Pictures/latest_4096_0304.jpg
 dconf write "/org/gnome/desktop/background/picture-uri" "'file:///home/notclaytonjohnson/Pictures/latest_4096_0304.jpg'"
-dconf write "/org/gnome/desktop/background/picture-options" "scaled"
-
-if [[ -f latest_4096_0171.jpg ]] ; then
-  rm latest_4096_0171.jpg
-fi
-
-wget https://sdo.gsfc.nasa.gov/assets/img/latest/latest_4096_0171.jpg
-cp latest_4096_0171.jpg /home/notclaytonjohnson/Pictures/Sun/0171/latest_4096_0171_$(date -u --iso-8601=minutes).jpg
-mv latest_4096_0171.jpg /home/notclaytonjohnson/Pictures/latest_4096_0171.jpg
+dconf write "/org/gnome/desktop/background/picture-options" "'scaled'"
